39d8a998caaccf24882935153234c9228e65bd35
Artur Aquino <80642826+aquinoartur@users.noreply.github.com>
2023-08-06 14:55:43 -0300

    firebaseconfig

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'firebase/prod/firebase_options.dart' as prd;
+import 'firebase/dev/firebase_options.dart' as dev;
 
-void main() {
+// Flavors
+enum Flavor { dev, stg, prd }
+
+// Environment
+const dimension = String.fromEnvironment('dimension');
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: dimension != Flavor.prd.name
+        ? dev.DefaultFirebaseOptions.currentPlatform
+        : prd.DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const MainApp());
 }
 

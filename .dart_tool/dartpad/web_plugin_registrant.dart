59f975ddd3f623abfad9ec15339ef2357703cd0b
Artur Aquino <80642826+aquinoartur@users.noreply.github.com>
2023-08-06 15:05:23 -0300

    flutter dependencies

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -11,6 +11,7 @@ import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:firebase_database_web/firebase_database_web.dart';
 import 'package:firebase_remote_config_web/firebase_remote_config_web.dart';
 import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
@@ -20,5 +21,6 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   FirebaseDatabaseWeb.registerWith(registrar);
   FirebaseRemoteConfigWeb.registerWith(registrar);
   FirebaseStorageWeb.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }

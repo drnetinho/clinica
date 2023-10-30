771a5b84d97fb8509f426a7b4a53be720153f346
adelmo aquino <80642826+aquinoartur@users.noreply.github.com>
2023-10-30 10:36:54 -0300

    removed cached image

--- lib/core/components/medical_scale_card_widget_mobile.dart
+++ lib/core/components/medical_scale_card_widget_mobile.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:netinhoappclinica/core/components/state_widget.dart';
@@ -165,7 +164,7 @@ class _MedicalScaleCardWidgetMobileState extends State<MedicalScaleCardWidgetMob
                     children: [
                       CircleAvatar(
                         radius: 26,
-                        backgroundImage: CachedNetworkImageProvider(doctor.image),
+                        backgroundImage: NetworkImage(doctor.image),
                       ),
                       const SizedBox(width: 20),
                       Column(
